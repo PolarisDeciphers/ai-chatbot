c3a8ca81b9ad1fe48ffbb2445c80e632485d5171
PolarisDeciphers <Dhruvsra64b@gmail.com>
2025-06-15 16:21:55 +0530

    Delete components directory

--- components/FileUploader.tsx
+++
@@ -1,9 +0,0 @@
-
-export default function FileUploader() {
-  return (
-    <div style={{ marginTop: '2rem' }}>
-      <label>Upload a file (PDF/DOCX/TXT): </label>
-      <input type="file" />
-    </div>
-  );
-}
