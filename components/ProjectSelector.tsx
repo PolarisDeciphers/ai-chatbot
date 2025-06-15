d73d6deb74c3e0e68a628d87c13a12e0eea0d439
PolarisDeciphers <Dhruvsra64b@gmail.com>
2025-06-15 16:16:22 +0530

    Create ProjectSelector.tsx

---
+++ components/ProjectSelector.tsx
@@ -0,0 +1,11 @@
+
+export default function ProjectSelector() {
+  return (
+    <div style={{ marginTop: '2rem' }}>
+      <label>Select Project: </label>
+      <select>
+        <option value="default">Default Project</option>
+      </select>
+    </div>
+  );
+}
