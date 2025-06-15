87597b4ab4263c8600f6c5f7049d3aa0a7eb7011
PolarisDeciphers <Dhruvsra64b@gmail.com>
2025-06-15 16:20:05 +0530

    Create index.tsx

---
+++ pages/index.tsx
@@ -0,0 +1,21 @@
+
+import Head from 'next/head';
+import ChatBox from '../components/ChatBox';
+import FileUploader from '../components/FileUploader';
+import ProjectSelector from '../components/ProjectSelector';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>CS Co-Pilot</title>
+      </Head>
+      <main style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+        <h1>Welcome to CS Co-Pilot</h1>
+        <ProjectSelector />
+        <FileUploader />
+        <ChatBox />
+      </main>
+    </>
+  );
+}
